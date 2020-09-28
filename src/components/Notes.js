13d4d31ba1c87ec48685d762ca71b17c97c9bc00
Tatsiana Zubel <tatsiana.zubel@icloud.com>
2020-09-28 16:30:27 +0300

    Added Bootstrap’s grid system to Notes component.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -9,35 +9,35 @@ export const Notes = ({ notes, onRemove, onEdit }) => {
                     className="list-group-item note"
                     key={note.id}
                 >
-                    <div >
-                        <small>{new Date().toLocaleDateString()}</small>
-                        &nbsp;<strong>{note.name}</strong>
-                        <ContentEditable
-                            html={note.message}
-                            disabled={!note.editable}
-                            className={note.editable ? 'message editable' : 'message'}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <button
-                            type="button"
-                            className="btn btn-info m-2"
-                            onClick={() => onEdit(note)}
-                        >
-                        {note.editable
-                            ? 'Save'
-                            : 'Edit'
-                        }
-                        </button>
-                        <button
-                            type="button"
-                            className="btn btn-danger m-2"
-                            onClick={() => onRemove(note.id)}
-                        >
-                            &times;
-                        </button>
-
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-10">
+                                <small>{new Date().toLocaleDateString()}</small>
+                                &nbsp;<strong>{note.name}</strong>
+                                <ContentEditable
+                                    html={note.message}
+                                    disabled={!note.editable}
+                                    className={note.editable ? 'message editable' : 'message'}
+                                // onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="col-2">
+                                <button
+                                    type="button"
+                                    className="btn btn-info m-2"
+                                    onClick={() => onEdit(note)}
+                                >
+                                    {note.editable ? 'Save' : 'Edit'}
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-danger m-2"
+                                    onClick={() => onRemove(note.id)}
+                                >
+                                &times;
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </li>
             ))}
